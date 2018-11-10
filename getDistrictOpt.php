a7d49ef48f140b869f4d7786d2711d6b445616f5
goodvubo <goodvubo@gmail.com>
2018-11-11 01:42:30 +0600

    no problems here, getList maybe buggy

--- getDistrictOpt.php
+++ getDistrictOpt.php
@@ -5,17 +5,17 @@ $_q1 = mysqli_real_escape_string($conn, $_GET['q1']);
 
 $seletSql = "SELECT DISTINCT district FROM " . $_q1;
 
-echo $_q1 . "<br />";
-echo $seletSql . "<br />";
+//echo $_q1 . "<br />";
+//echo $seletSql . "<br />";
 
 $result = $conn->query($seletSql);
 
-echo "<option value='' selected>Select District</option>";
-echo "<option value='null'>Select District</option>";
+echo "<option value='' disabled selected>Select District</option>";
+echo "<option value='null'>Any District</option>";
 if ($result->num_rows > 0) {
     while ($rows = $result->fetch_assoc()) {
         if($rows['district'] != '')
-        echo "<select value='" . strtolower($rows['district']) . "' >" . $rows['district'] . "</select>";
+        echo "<option value='" . strtolower($rows['district']) . "' >" . $rows['district'] . "</option>";
     }
 }
 
